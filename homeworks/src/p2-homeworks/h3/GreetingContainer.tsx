5b054946f761052275cf4b88b93dad4d02b8ff81
Roman Likhomanov <romanlee87@gmail.com>
2022-06-15 11:13:20 +0300

    task-3

--- homeworks/src/p2-homeworks/h3/GreetingContainer.tsx
+++ homeworks/src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from './HW3';
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: UserType[]
+    addUserCallback: (name: string) => void
+    totalU: number
 }
 
 // более простой и понятный для новичков
@@ -11,18 +13,33 @@ type GreetingContainerPropsType = {
 
 // более современный и удобный для про :)
 // уровень локальной логики
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback, totalU
+}) => { // деструктуризация пропсов
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+    const setNameCallback = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.currentTarget.value || e.currentTarget.value === ' ') {
+            setError('name is requaier!')
+            setName('')
+        } else {
+            setName(e.currentTarget.value)
+            setError('')
+        }
     }
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        addUserCallback(name)
+        alert(`Hello ${name} !`)
+        setName('')
     }
 
-    const totalUsers = 0 // need to fix
+    const onEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter' && name) {
+            addUser()
+        }
+    }
+
+    const totalUsers = totalU // need to fix
 
     return (
         <Greeting
@@ -31,6 +48,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
             addUser={addUser}
             error={error}
             totalUsers={totalUsers}
+            onEnter={onEnter}
         />
     )
 }
--- homeworks/src/p2-homeworks/h3/HW3.tsx
+++ homeworks/src/p2-homeworks/h3/HW3.tsx
@@ -1,27 +1,31 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v1} from 'uuid';
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string
+    name: string
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<UserType[]>([])
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+    const addUserCallback = (name: string) => {
+        const newUser = {_id: v1(), name: name}
+        setUsers([newUser, ...users])
     }
 
+    let totalU = users.length
+console.log(users)
     return (
         <div>
             <hr/>
             homeworks 3
 
             {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+            <GreetingContainer users={users} addUserCallback={addUserCallback} totalU={totalU}/>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
