9763c5eead8746619541d5da44b69bd0360d12af
Roman Likhomanov <romanlee87@gmail.com>
2022-07-02 12:26:29 +0300

    task-5

--- homeworks/src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ homeworks/src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from 'react-router-dom';
+import s from './Header.module.css';
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+    const [open, setOpen] = useState(false)
 
+    const openHandler = () => {
+        setOpen(!open)
+    }
+
+    return (
+        <div className={s.root}>
+            {open && (
+            <div className={s.nav}>
+                <NavLink to={'/pre-junior'} className={(navData) => navData.isActive ? s.active : s.link }>pre-junior</NavLink>
+                <NavLink to={'/junior'} className={(navData) => navData.isActive ? s.active : s.link }>junior</NavLink>
+                <NavLink to={'/junior-plus'} className={(navData) => navData.isActive ? s.active : s.link }>junior+</NavLink>
+            </div>)
+            }
+            <div onMouseEnter={openHandler} className={s.square}></div>
         </div>
     )
 }
