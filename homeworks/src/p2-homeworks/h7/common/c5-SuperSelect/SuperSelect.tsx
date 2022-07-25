7e7d3499d83d7db773fe7a896a61efca8bb3dc4d
Roman Likhomanov <romanlee87@gmail.com>
2022-07-25 11:26:35 +0300

    task-7

--- homeworks/src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ homeworks/src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -14,9 +14,14 @@ const SuperSelect: React.FC<SuperSelectPropsType> = (
         ...restProps
     }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+
+    const mappedOptions: any[] = options ? options.map((el, i) => (
+        <option key={el+'-'+i} value={el}>{el}</option>
+    )): []// map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
         // onChange, onChangeOption
     }
 
