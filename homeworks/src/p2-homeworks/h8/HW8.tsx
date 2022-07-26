0b4b5507513123fb7e6b75b1ec24ea7049a1032c
Roman Likhomanov <romanlee87@gmail.com>
2022-07-26 14:43:05 +0300

    task-8

--- homeworks/src/p2-homeworks/h8/HW8.tsx
+++ homeworks/src/p2-homeworks/h8/HW8.tsx
@@ -1,10 +1,17 @@
 import React, {useState} from 'react'
 import {homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './HW8.module.css'
 
-// export type UserType =
+type UserType = {
+    _id: number
+    name: string
+    age: number
+}
 
-const initialPeople = [
+export type InitialPeopleType = UserType[]
+
+const initialPeople: InitialPeopleType = [
     {_id: 0, name: 'Кот', age: 3},
     {_id: 1, name: 'Александр', age: 66},
     {_id: 2, name: 'Коля', age: 16},
@@ -14,16 +21,19 @@ const initialPeople = [
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
-
+    const [people, setPeople] = useState<InitialPeopleType>(initialPeople) // need to fix any
+console.log(people)
     // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
+    const finalPeople = people.map((p: UserType) => (
+        <div key={p._id} className={s.peopleBox}>
+            <div className={s.people}>{p.name}</div>
+            <div className={s.people}>{p.age}</div>
         </div>
     ))
 
     const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
+    const check = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 18}))
 
     return (
         <div>
@@ -32,11 +42,11 @@ function HW8() {
 
             {/*should work (должно работать)*/}
             {finalPeople}
-
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
-
+            <div className={s.buttonBox}>
+                <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
+                <div><SuperButton onClick={sortDown}>sort down</SuperButton></div>
+                <div><SuperButton onClick={check}>check 18</SuperButton></div>
+            </div>
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativePeople/>*/}
--- homeworks/src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ homeworks/src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,24 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {InitialPeopleType} from '../HW8';
+
+type ActionType = { type: 'sort', payload: 'up' | 'down' } | { type: 'check', payload: number }
+
+
+export const homeWorkReducer = (state: InitialPeopleType, action: ActionType): InitialPeopleType => { // need to fix any
     switch (action.type) {
-        case 'sort': {
+        case 'sort':
             // need to fix
-            return state
-        }
+            const newState = [...state].sort((a, b) => {
+                if(a.name > b.name) return 1
+                else if(a.name < b.name) return -1
+                else return 0
+            })
+            return action.payload === 'up' ? newState : newState.reverse()
+
         case 'check': {
             // need to fix
-            return state
+            return state.filter(el => el.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
