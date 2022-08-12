b62d08d75195bfb9a62deddde68c23f579dd91e5
Roman Likhomanov <romanlee87@gmail.com>
2022-08-12 16:59:08 +0300

    task-10

--- homeworks/src/p2-homeworks/h10/HW10.tsx
+++ homeworks/src/p2-homeworks/h10/HW10.tsx
@@ -1,27 +1,42 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from 'react-redux';
+import {AppStoreType} from './bll/store';
+import {loadingAC} from './bll/loadingReducer';
+import preloader from './Spinner.svg'
 
 function HW10() {
     // useSelector, useDispatch
-    const loading = false
+    const loading = useSelector<AppStoreType, { isLoading: boolean }>(state => state.loading)
+    const dispatch = useDispatch();
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
-        console.log('loading...')
+        dispatch(loadingAC(true))
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 3000)
     };
 
     return (
         <div>
             <hr/>
             homeworks 10
-
             {/*should work (должно работать)*/}
-            {loading
+            {loading.isLoading
                 ? (
-                    <div>крутилка...</div>
+                    <div style={{width: '10px', height: '10px'}}>
+                        <img src={preloader}/>
+                    </div>
+                    // <div>крутилка...</div>
                 ) : (
                     <div>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
                     </div>
                 )
--- homeworks/src/p2-homeworks/h10/bll/loadingReducer.ts
+++ homeworks/src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,24 @@
-const initState = {
+import {AppStoreType} from './store';
 
+export type ActionLoadingTypes = ReturnType<typeof loadingAC>
+
+const TOGGLE_IS_LOADING = 'TOGGLE-IS-LOADING'
+
+const initState = {
+isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionLoadingTypes): {isLoading: boolean} => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case TOGGLE_IS_LOADING: {
+            return {
+                ...state, isLoading: action.isLoading
+            }
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (isLoading: boolean) => ({ type: TOGGLE_IS_LOADING, isLoading}) as const
+ // fix any
+
